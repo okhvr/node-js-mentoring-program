0c68c4813dacede14af53c7e33df40a8b8f03b82
Olha Katsalap <olg.katsalap@gmail.com>
2019-11-23 20:32:52 +0200

    add eslint and fix

--- src/task1.js
+++ src/task1.js
@@ -2,10 +2,10 @@ import readline from 'readline';
 
 const rl = readline.createInterface({
   input: process.stdin,
-  output: process.stdout
+  output: process.stdout,
 });
 
 
-rl.on('line', function(line){
-    console.log(line.split("").reverse().join(""));
-})
+rl.on('line', (line) => {
+  console.log(line.split('').reverse().join(''));
+});
